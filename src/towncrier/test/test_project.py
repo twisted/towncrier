0a4c17eb92e959221b167798d2b5190a9b9dccd8
HawkOwl <hawkowl@atleastfornow.net>
2015-12-29 18:49:54 +0800

    version switch plus allowing tuples

---
+++ src/towncrier/test/test_project.py
@@ -0,0 +1,39 @@
+# Copyright (c) Amber Brown, 2015
+# See LICENSE for details.
+
+import os
+
+from twisted.trial.unittest import TestCase
+
+from .._project import get_version
+
+
+class VersionFetchingTests(TestCase):
+
+    def test_str(self):
+        """
+        A str __version__ will be picked up.
+        """
+        temp = self.mktemp()
+        os.makedirs(temp)
+        os.makedirs(os.path.join(temp, "mytestproj"))
+
+        with open(os.path.join(temp, "mytestproj", "__init__.py"), "w") as f:
+            f.write("__version__ = '1.2.3'")
+
+        version = get_version(temp, "mytestproj")
+        self.assertEqual(version, "1.2.3")
+
+    def test_tuple(self):
+        """
+        A tuple __version__ will be picked up.
+        """
+        temp = self.mktemp()
+        os.makedirs(temp)
+        os.makedirs(os.path.join(temp, "mytestproj"))
+
+        with open(os.path.join(temp, "mytestproj", "__init__.py"), "w") as f:
+            f.write("__version__ = (1, 3, 12)")
+
+        version = get_version(temp, "mytestproj")
+        self.assertEqual(version, "1.3.12")
