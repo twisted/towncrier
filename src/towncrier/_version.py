0df355965387577e0cd97c1254324f0b506c03d5
Amber Brown (HawkOwl) <hawkowl@atleastfornow.net>
2016-12-07 21:37:18 +1100

    version

--- src/towncrier/_version.py
+++ src/towncrier/_version.py
@@ -1,4 +1,11 @@
+"""
+Provides towncrier version information.
+"""
+
+# This file is auto-generated! Do not edit!
+# Use `python -m incremental.update towncrier` to change this file.
+
 from incremental import Version
 
-__version__ = Version("towncrier", 16, 1, 0)
+__version__ = Version('towncrier', 16, 12, 0)
 __all__ = ["__version__"]
