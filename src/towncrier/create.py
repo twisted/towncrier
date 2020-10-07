697d16f1cfa73f15cdf4eaddc338e669bde32a94
Sagi Shadur <saroad2@gmail.com>
2020-10-07 23:12:21 +0300

    Users can now write the content in the "create" method.

--- src/towncrier/create.py
+++ src/towncrier/create.py
@@ -16,12 +16,13 @@ from ._settings import load_config_from_options
 @click.command(name="create")
 @click.option("--dir", "directory", default=None)
 @click.option("--config", "config", default=None)
+@click.option("-i", "--interactive", is_flag=True, default=False)
 @click.argument("filename")
-def _main(directory, config, filename):
-    return __main(directory, config, filename)
+def _main(directory, config, filename, interactive):
+    return __main(directory, config, filename, interactive)
 
 
-def __main(directory, config, filename):
+def __main(directory, config, filename, interactive):
     """
     The main entry point.
     """
@@ -59,10 +60,29 @@ def __main(directory, config, filename):
     if os.path.exists(segment_file):
         raise click.ClickException("{} already exists".format(segment_file))
 
+    content = _get_news_content(interactive)
+
     with open(segment_file, "w") as f:
-        f.writelines(["Add your info here"])
+        f.write(content)
 
     click.echo("Created news fragment at {}".format(segment_file))
+
+
+def _get_news_content(interactive):
+    if not interactive:
+        return "Add your info here"
+    content = []
+    click.echo(
+        "Please write your news content. Press enter without content to finish"
+    )
+    count = 1
+    while True:
+        content_line = click.prompt("{}".format(count), default="", show_default=False)
+        if content_line == "":
+            break
+        content.append(content_line)
+        count += 1
+    return "\n".join(content)
 
 
 if __name__ == "__main__":  # pragma: no cover
