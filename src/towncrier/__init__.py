133516ef0bf00ca30970e13ef2505ec61b69e0c2
Kyle Altendorf <sda@fstab.net>
2020-12-05 22:20:58 -0500

    Revert "Revert "revert this intentional failure""
    This reverts commit 05fec2af9a668521f557506a2e63ec2660475a7c.

--- src/towncrier/__init__.py
+++ src/towncrier/__init__.py
@@ -8,3 +8,5 @@ towncrier, a builder for your news files.
 from ._version import __version__
 
 __all__ = ["__version__"]
+
+let_us_fail_lint = ["this", "that", "more", "and", "always", "but", "this", "is", "a", "silly", "way", "but", "it", "should", "work"]
