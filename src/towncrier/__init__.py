4c95802efd79981443f5823d33804454d139edb9
Kyle Altendorf <sda@fstab.net>
2020-12-06 23:18:58 -0500

    remove lint-failing example

--- src/towncrier/__init__.py
+++ src/towncrier/__init__.py
@@ -8,5 +8,3 @@ towncrier, a builder for your news files.
 from ._version import __version__
 
 __all__ = ["__version__"]
-
-let_us_fail_lint = ["this", "that", "more", "and", "always", "but", "this", "is", "a", "silly", "way", "but", "it", "should", "work"]
