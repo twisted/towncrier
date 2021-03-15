3aac85b020add1988a47e8b97bf8766d9ade27b8
Kyle Altendorf <sda@fstab.net>
2021-03-15 11:54:55 -0400

    Add build-system to pyproject.toml for PEP 517 (-m build) (#323)
    Co-authored-by: Thomas Grainger <tagrain@gmail.com>

--- setup.py
+++ setup.py
@@ -4,6 +4,11 @@ from __future__ import absolute_import, division, print_function
 
 from setuptools import setup, find_packages
 
+# If incremental is not present then setuptools just silently uses v0.0.0 so
+# let's import it and fail instead.
+import incremental
+
+
 setup(
     name="towncrier",
     maintainer="Amber Brown",
@@ -36,7 +41,6 @@ setup(
         "Programming Language :: Python :: Implementation :: PyPy",
     ],
     use_incremental=True,
-    setup_requires=["incremental"],
     install_requires=["click", "click-default-group", "incremental", "jinja2", "toml"],
     extras_require={"dev": ["packaging"]},
     package_dir={"": "src"},
