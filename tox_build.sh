4aeec253d79dc979fa9e249ad85be914b3777527
Kyle Altendorf <sda@fstab.net>
2021-03-18 14:49:29 -0400

    drop separate manifest and release checks

--- tox_build.sh
+++ tox_build.sh
@@ -2,9 +2,15 @@
 
 set -evx
 
-check-manifest ${toxinidir}
+check-manifest --verbose ${toxinidir}
+
 python -m build --sdist --outdir ${toxinidir}/dist/ ${toxinidir}
+
 tar -xvf ${toxinidir}/dist/*
 cd *
+
 # build the wheel from the sdist
 python -m build --wheel --outdir ${toxinidir}/dist/ .
+cd -
+
+twine check ${toxinidir}/dist/*
--- tox_check-release.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash
-
-set -evx
-
-twine check ${toxinidir}/dist/*
