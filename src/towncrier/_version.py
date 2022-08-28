f031dfa94e52e07b6c71e88cd5cd50e3803ad96c
Adi Roiban <adi.roiban@chevah.com>
2022-08-28 20:30:06 +0100

    towncrier build --yes

--- src/towncrier/_version.py
+++ src/towncrier/_version.py
@@ -8,5 +8,5 @@ Provides towncrier version information.
 from incremental import Version
 
 
-__version__ = Version("towncrier", 21, 9, 1, dev=0)
+__version__ = Version("towncrier", 22, 8, 0, release_candidate=1)
 __all__ = ["__version__"]
