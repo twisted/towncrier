7bb88f47210d4e3c78a631742dfedbdc34215a3a
Adi Roiban <adi.roiban@chevah.com>
2022-11-06 14:53:41 +0000

    Initial report for missing lines.

--- .github/scripts/pr_comment.js
+++ .github/scripts/pr_comment.js
@@ -7,41 +7,34 @@ Update the content of the existing comment.
 
 https://octokit.github.io/rest.js/v19
 */
-module.exports = async ({github, context, process}) => {
-    var octokit_rest = github
+module.exports = async ({github, context, process, retry_delay}) => {
+    // Create the namespace to make it easy to copy/paste example from the
+    // octokit docs.
+    const octokit = {rest: github}
+
+    const comment_marker = '\n' + process.env.COMMENT_MARKER
+
     if (context.eventName != "pull_request") {
         // Only PR are supported.
         return
     }
 
-    var sleep = function(second) {
+    var sleep = (second) => {
         return new Promise(resolve => setTimeout(resolve, second * 1000))
     }
 
     /*
-    Perform the actual logic.
-
-    This is wrapped so that we can retry on errors.
+    Create or update the PR summary report as a single comment.
     */
-    var doAction = async function() {
-
-        console.log(context)
-
-        fs = require('fs');
-
-        const body = fs.readFileSync(
-            process.env.GITHUB_WORKSPACE + "/" + process.env.COMMENT_BODY, 'utf8');
+    var doSummaryComment = async (body) => {
         var comment_id = null
-        var comment_marker = '\n' + process.env.COMMENT_MARKER
-        var comment_body = body + comment_marker
+        const comment_body = body + comment_marker
 
-        var comments = await octokit_rest.issues.listComments({
+        const comments = await octokit.rest.issues.listComments({
             owner: context.repo.owner,
             repo: context.repo.repo,
             issue_number: context.payload.number,
           })
-
-        console.log(comments)
 
         comments.data.forEach(comment => {
             if (comment.body.endsWith(comment_marker)) {
@@ -52,7 +45,7 @@ module.exports = async ({github, context, process}) => {
         if (comment_id) {
             // We have an existing comment.
             // update the content.
-            await octokit_rest.issues.updateComment({
+            await octokit.rest.issues.updateComment({
                 owner: context.repo.owner,
                 repo: context.repo.repo,
                 comment_id: comment_id,
@@ -62,7 +55,7 @@ module.exports = async ({github, context, process}) => {
         }
 
         // Create a new comment.
-        await octokit_rest.issues.createComment({
+        await octokit.rest.issues.createComment({
             owner: context.repo.owner,
             repo: context.repo.repo,
             issue_number: context.payload.number,
@@ -71,10 +64,82 @@ module.exports = async ({github, context, process}) => {
 
     }
 
+    /*
+    Create or refresh the diff inline comments.
+    */
+    var doDiffComments = async (report) => {
+
+        var review_id = null
+        const existing_reviews = await octokit.rest.pulls.listReviews({
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+            pull_number: context.payload.number,
+        })
+        existing_reviews.data.forEach((review) => {
+            if (review.body.endsWith(comment_marker)) {
+                review_id = review.id
+            }
+        })
+
+        if (review_id) {
+            // We have an existing review.
+            // Delete it as we will replace it with a new one.
+
+        }
+
+        // Prepare inline comments.
+        var comments = []
+        Object.keys(report.src_stats).forEach((path) => {
+            report.src_stats[path].violation_lines.forEach((position) => {
+                comments.push({
+                    path,
+                    position,
+                    body: 'Missing coverage.'
+                })
+            })
+          })
+
+        if (!comments) {
+            // Coverage is complete. Nothing to comment about.
+            return
+        }
+
+        await octokit.rest.pulls.createReview({
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+            commit_id: context.payload.after,
+            pull_number: context.payload.number,
+            event: "COMMENT",
+            body: "Missing coverage report." + comment_marker,
+            comments
+        })
+    }
+
+    /*
+    Perform the actual logic.
+
+    This is wrapped so that we can retry on errors.
+    */
+    var doAction = async () => {
+
+        console.log(context)
+
+        fs = require('fs');
+
+        const body = fs.readFileSync(
+            process.env.GITHUB_WORKSPACE + "/" + process.env.COMMENT_BODY, 'utf8')
+        await doSummaryComment(body)
+
+        const report_json = fs.readFileSync(
+            process.env.GITHUB_WORKSPACE + "/" + process.env.REPORT_JSON, 'utf8');
+        await doDiffComments(JSON.parse(report_json))
+
+    }
+
     try {
         await doAction()
     } catch (e) {
-        await sleep(5)
+        await sleep(retry_delay)
         await doAction()
     }
 }
---
+++ .github/scripts/pr_comment.test.js
@@ -0,0 +1,72 @@
+/*
+This should be called via nodejs from the repo root folder.
+
+node .github/scripts/pr_comment.test.js
+
+This is just a poor man's testing code for the script.
+Since the project is mostly python, is not easy to add nodeenv and a full
+testing suite like jest.
+*/
+const process = require('process');
+
+process.env.GITHUB_WORKSPACE = process.cwd()
+process.env.COMMENT_BODY = '.github/scripts/pr_comment.test.body.txt'
+process.env.REPORT_JSON = '.github/scripts/pr_comment.test.report.json'
+process.env.COMMENT_MARKER = '<!--- comment-marker -->'
+
+const script = require(process.cwd() + '/.github/scripts/pr_comment.js')
+const retry_delay = 0.1
+
+var github = {
+    issues: {
+        listComments: (options) => {
+            console.log('Retrieving comments...')
+            console.log(options)
+            return {data: [
+                {'id': 980, 'body': 'some-other-comment'},
+                // Comment the line below to trigger creating a new comment.
+                {'id': 456, 'body': 'comment marker\n<!--- comment-marker -->'},
+                {'body': 'another-other-comment'},
+
+            ]}
+        },
+        updateComment: (options) => {
+            console.log('Updating existing comment...')
+            console.log(options)
+        },
+        createComment: (options) => {
+            console.log('Creating new comment...')
+            console.log(options)
+        },
+    },
+    pulls: {
+        listReviews: (options) => {
+            console.log('Listing existing revies...')
+            console.log(options)
+            return {data: [
+                {'id': 120, 'body': 'some-other-pr-review'},
+                // Comment the line below to trigger creating a new comment.
+                {'id': 156, 'body': 'review marker\n<!--- comment-marker -->'},
+            ]}
+        },
+        createReview: (options) => {
+            console.log('Creating new review...')
+            console.log(options)
+        },
+    },
+}
+
+var context = {
+    eventName: 'pull_request',
+    repo: {
+        owner: 'dummy-org',
+        repo: 'dummy-repo',
+    },
+    payload: {
+        number: 123,
+        after: '123abc567',
+    }
+}
+
+
+script({github, context, process, retry_delay})
