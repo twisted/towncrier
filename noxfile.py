9554985492bb023482f0596b668d11bc7351c6ab
Adi Roiban <adi.roiban@chevah.com>
2022-11-06 14:46:47 +0000

    Generate coverage reports using only GitHub Actions, without codecov.io (#447)
    * Initial coverage report.
    
    * Enable for windows.
    
    * Try to support windows paths for coverage.
    
    * Try the branch.
    
    * Update all-success to newer version that as no set-output warnings.
    
    * Add a comment.
    
    * Pass comment via a file to handle newlines.
    
    * Use nodejs like a pro.
    
    * Apply suggestions from code review
    
    Co-authored-by: Kyle Altendorf <sda@fstab.net>
    
    * Include diff. Wrap in markdown.
    
    * Enable test report.
    
    * Fix after re-review.
    
    * Remove run_id from coverage artifact.
    
    * Add a name for the comment script.
    
    * Remove comment about single artifact as this is what we have by default for a workflow.
    
    * Add restricted permissions for token.
    
    * Use markdown report for diff-cover.
    
    Co-authored-by: Kyle Altendorf <sda@fstab.net>

--- noxfile.py
+++ noxfile.py
@@ -36,9 +36,8 @@ def tests(session: nox.Session) -> None:
     session.run("coverage", "run", "--module", "twisted.trial", *posargs)
 
     if os.environ.get("CI") != "true":
+        # When running the test locally, show the coverage report.
         session.notify("coverage_report")
-    else:
-        session.run("coverage", "combine")
 
 
 @nox.session
