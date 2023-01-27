ed205d3e6f81300e02e64dd1ba9e8ac8b2235617
Hynek Schlawack <hs@ox.cx>
2023-01-27 19:08:32 +0100

    Update src/towncrier/_version.py
    Co-authored-by: Adi Roiban <adi.roiban@chevah.com>

--- src/towncrier/_version.py
+++ src/towncrier/_version.py
@@ -9,6 +9,8 @@ from incremental import Version
 
 
 __version__ = Version("towncrier", 22, 12, 1, dev=0)
+# The version is exposed in string format to be
+# available for the hatching build tools.
 _hatchling_version = __version__.short()
 
 __all__ = ["__version__", "_hatchling_version"]
