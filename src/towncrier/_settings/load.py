80be0150d5dee6165cf6407e73575705784e6c2d
Chris Beaven <smileychris@gmail.com>
2024-05-23 10:07:39 +1200

    Add docstring to load_config_from_options and some explanatory comments of the different branch behaviour.

--- src/towncrier/_settings/load.py
+++ src/towncrier/_settings/load.py
@@ -65,19 +65,32 @@ class ConfigError(ClickException):
 def load_config_from_options(
     directory: str | None, config_path: str | None
 ) -> tuple[str, Config]:
+    """
+    Load the configuration from the given directory or specific configuration file.
+
+    Returns a tuple of the base directory and the parsed Config instance.
+    """
     if config_path is None:
         if directory is None:
+            # Neither a directory or explicit config path was given so traverse for a
+            # config.
             return traverse_for_config(None)
 
+        # Since no explicit config path was provided, we'll look for a config in the
+        # given directory.
         base_directory = os.path.abspath(directory)
         config = load_config(base_directory)
     else:
+        # If an explicit config path was provided, we'll use that.
         config_path = os.path.abspath(config_path)
+        config = load_config_from_file(os.path.dirname(config_path), config_path)
+
+        # If a directory was also provided, we'll use that as the base directory
+        # otherwise we'll use the directory containing the config file.
         if directory is None:
             base_directory = os.path.dirname(config_path)
         else:
             base_directory = os.path.abspath(directory)
-        config = load_config_from_file(os.path.dirname(config_path), config_path)
 
     if config is None:
         raise ConfigError(f"No configuration file found.\nLooked in: {base_directory}")
