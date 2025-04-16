a706bcf6926dd8279297dc6473e81d7169c97197
Christophe de Vienne <cdevienne@users.noreply.github.com>
2025-04-16 11:44:36 +0200

    Apply suggestions from code review
    Co-authored-by: Adi Roiban <adiroiban@gmail.com>

--- src/towncrier/test/test_novcs.py
+++ src/towncrier/test/test_novcs.py
@@ -5,6 +5,9 @@ from towncrier import _novcs
 
 class TestNOVCS(TestCase):
     def test_get_default_compare_branch(self):
+        """
+        Witout a version control system, there is no default branch.
+        """
         self.assertEqual(None, _novcs.get_default_compare_branch([]))
 
     def test_empty_remove(self):
@@ -14,9 +17,15 @@ class TestNOVCS(TestCase):
         _novcs.remove_files([])
 
     def test_get_remote_branches(self):
+        """
+        There are no remote branches, when we don't have a VCS.
+        """
         self.assertEqual([], _novcs.get_remote_branches("."))
 
     def test_list_changed_files_compared_to_branch(self):
+        """
+        No changed files are detected without a VCS.
+        """
         self.assertEqual(
             [], _novcs.list_changed_files_compared_to_branch(".", "main", False)
         )
